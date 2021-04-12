42765320be3d3831af8901406f12e01269fbc669
Najika Yoo <halsema@najikas-mbp.sea.adobe.com>
2021-04-12 14:43:41 -0700

    chore: reformatting demo

--- projects/example-project-rollup/src/example-app.ts
+++ projects/example-project-rollup/src/example-app.ts
@@ -12,18 +12,20 @@ governing permissions and limitations under the License.
 
 // import our stylesheets
 import '@spectrum-web-components/theme/sp-theme.js';
+
+// For Express
 import '@spectrum-web-components/theme/src/diet/core.js';
 import '@spectrum-web-components/theme/diet/theme-light.js';
 import '@spectrum-web-components/theme/diet/scale-medium.js';
 
-import '@spectrum-web-components/theme/theme-dark';
+// For Classic
 import '@spectrum-web-components/theme/theme-light';
-
 import '@spectrum-web-components/theme/scale-medium';
 
 // import the components we'll use in this page
 import '@spectrum-web-components/action-button/sp-action-button';
 import '@spectrum-web-components/action-group/sp-action-group.js';
 import '@spectrum-web-components/button/sp-button';
+import '@spectrum-web-components/button-group/sp-button-group';
 import '@spectrum-web-components/field-label/sp-field-label';
 import '@spectrum-web-components/textfield/sp-textfield';
