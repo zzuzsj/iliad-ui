030409f75793b0bc9122a64b7849526c6ee4b102
zzuzsj <zzu2012zhousj@163.com>
2021-11-01 18:42:32 +0800

    feat: 增加icons-figma包，通过figma信息拉取最新的icon信息并打包成为wc组件

---
+++ packages/icons-figma/src/index.ts
@@ -0,0 +1,22 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+Copyright 2021 Gaoding. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+export { IconBorder } from './elements/Border.js';
+export { IconFormatCenter } from './elements/FormatCenter.js';
+export { IconFormatJustified } from './elements/FormatJustified.js';
+export { IconFormatLeft } from './elements/FormatLeft.js';
+export { IconFormatRight } from './elements/FormatRight.js';
+export { IconTextBold } from './elements/TextBold.js';
+export { IconTextDeleteline } from './elements/TextDeleteline.js';
+export { IconTextItaly } from './elements/TextItaly.js';
+export { IconTextScale } from './elements/TextScale.js';
+export { IconTextUnderline } from './elements/TextUnderline.js';
