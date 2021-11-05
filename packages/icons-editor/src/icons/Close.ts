40e02eb842c0463ee10ff8f0b91287d421877212
zzuzsj <zzu2012zhousj@163.com>
2021-11-05 15:22:31 +0800

    feat: 新增编辑器icon库

---
+++ packages/icons-editor/src/icons/Close.ts
@@ -0,0 +1,31 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+Copyright 2021 Gaoding. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import { tag as html, TemplateResult } from '../custom-tag.js';
+export { setCustomTemplateLiteralTag } from '../custom-tag.js';
+export const SvgClose = (): string | TemplateResult => {
+    return html`
+        <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <path
+                d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                fill="#1F1F1F"
+            />
+        </svg>
+    `;
+};
