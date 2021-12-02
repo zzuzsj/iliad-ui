159947e84827af10304b4e1d61700e8001dbe2b2
zzuzsj <zzu2012zhousj@163.com>
2021-12-02 14:12:57 +0800

    style: copyright更新

--- packages/icons-editor/stories/icon-manifest.ts
+++ packages/icons-editor/stories/icon-manifest.ts
@@ -9,7 +9,8 @@ Unless required by applicable law or agreed to in writing, software distributed 
 the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
-*/ import { html, TemplateResult } from '@iliad-ui/base';
+*/
+import { html, TemplateResult } from '@iliad-ui/base';
 
 import '../icons/sp-icon-add';
 
