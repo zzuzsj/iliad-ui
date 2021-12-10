86ba17382bfea0f9ed2459c7d1f37845caf3d8a4
zzuzsj <zzu2012zhousj@163.com>
2021-12-10 09:55:23 +0800

    ci: custom-elements.json修改

--- scripts/add-custom-properties.js
+++ scripts/add-custom-properties.js
@@ -2,6 +2,7 @@
 
 /*
 Copyright 2020 Adobe. All rights reserved.
+Copyright 2021 Gaoding. All rights reserved.
 This file is licensed to you under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License. You may obtain a copy
 of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -57,7 +58,7 @@ async function main() {
                 }
             });
         });
-        customElementJsonString = JSON.stringify(customElementJson.modules);
+        customElementJsonString = JSON.stringify(customElementJson);
         fs.writeFileSync(inputCEJPath, customElementJsonString, {
             encoding: 'utf8',
         });
