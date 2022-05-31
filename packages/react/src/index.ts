c5dd7775d5f3b88b6a5af643e6771d3ecf3fd749
zzuzsj <zzu2012zhousj@163.com>
2022-05-31 14:26:09 +0800

    feat: react index文件移入src

---
+++ packages/react/src/index.ts
@@ -0,0 +1,91 @@
+/*
+Copyright 2020 Adobe. All rights reserved.
+Copyright 2021 Gaoding. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+/* 注册 web components sideEffect */
+import '@iliad-ui/bundle/elements';
+
+/* 脚本自动生成 */
+export * from './Accordion';
+export * from './AccordionItem';
+export * from './ActionBar';
+export * from './ActionButton';
+export * from './ActionMenu';
+export * from './ActionGroup';
+export * from './Asset';
+export * from './Avatar';
+export * from './Banner';
+export * from './Breadcrumb';
+export * from './Button';
+export * from './ClearButton';
+export * from './ButtonGroup';
+export * from './Card';
+export * from './Checkbox';
+export * from './Coachmark';
+export * from './ColorArea';
+export * from './ColorHandle';
+export * from './ColorLoupe';
+export * from './ColorSlider';
+export * from './ColorWheel';
+export * from './Dialog';
+export * from './DialogWrapper';
+export * from './Divider';
+export * from './Dropzone';
+export * from './FieldGroup';
+export * from './FieldLabel';
+export * from './Icon';
+export * from './IllustratedMessage';
+export * from './Meter';
+export * from './Link';
+export * from './NumberField';
+export * from './Menu';
+export * from './MenuDivider';
+export * from './MenuGroup';
+export * from './MenuItem';
+export * from './PickerBase';
+export * from './Panel';
+export * from './Popover';
+export * from './ProgressBar';
+export * from './ActiveOverlay';
+export * from './OverlayTrigger';
+export * from './ProgressCircle';
+export * from './QuickActions';
+export * from './Radio';
+export * from './RadioGroup';
+export * from './Search';
+export * from './SideNav';
+export * from './SideNavHeading';
+export * from './SideNavItem';
+export * from './Slider';
+export * from './SliderHandle';
+export * from './SplitView';
+export * from './StatusLight';
+export * from './Switch';
+export * from './Tab';
+export * from './TabPanel';
+export * from './Tabs';
+export * from './Textfield';
+export * from './Tag';
+export * from './Tags';
+export * from './Theme';
+export * from './Thumbnail';
+export * from './Toast';
+export * from './Tooltip';
+export * from './TopNav';
+export * from './TopNavItem';
+export * from './Tray';
+export * from './TreeItem';
+export * from './Underlay';
+
+/** 手动添加 */
+export * from './Picker';
+export * from './IconsEditor';
+export { Overlay } from '@iliad-ui/bundle';
